c2f5b497f021db19073a090610d494688e4fe3d6
Gene Koval <genyaremchuk@protonmail.com>
2021-10-05 17:26:41 -0400

    Initial Commit

---
+++ Shared/MintyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintyUIApp.swift
+//  Shared
+//
+//  Created by Gene Koval on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MintyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
