028188da90fff292842414b056272ff516296eb0
Gene Koval <genyaremchuk@protonmail.com>
2022-01-04 15:32:32 -0500

    Organize PostDetail using ViewBuilder properties

--- Shared/Views/Posts/PostDetail.swift
+++ Shared/Views/Posts/PostDetail.swift
@@ -12,65 +12,7 @@ struct PostDetail: View {
 
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                if let title = post.title {
-                    Text(title)
-                        .bold()
-                        .font(.title2)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-
-                if let description = post.description {
-                    Text(description)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-
-                Timestamp(
-                    prefix: "Posted",
-                    systemImage: "clock",
-                    date: post.created
-                )
-
-                if post.created != post.modified {
-                    Timestamp(
-                        prefix: "Last modified",
-                        systemImage: "pencil",
-                        date: post.modified
-                    )
-                }
-
-                if post.tags.count > 1 {
-                    NavigationLink(destination: TagList(post: post)) {
-                        Label(
-                            post.tags.countOf(type: "Tag"),
-                            systemImage: "tag"
-                        )
-                        .font(.caption)
-                    }
-                }
-                else if let tag = post.tags.first {
-                    NavigationLink( destination: TagDetail(
-                        tag: tag,
-                        repo: post.repo
-                    )) {
-                        Label(tag.name, systemImage: "tag")
-                            .font(.caption)
-                    }
-                }
-
-                if !post.objects.isEmpty {
-                    Label(
-                        post.objects.countOf(type: "Object"),
-                        systemImage: "doc"
-                    )
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                    .padding(.bottom, 10)
-
-                    ObjectGrid(objects: post.objects)
-                }
-            }
-            .padding()
+            postInfo
         }
         .navigationTitle("Post")
         .navigationBarTitleDisplayMode(.inline)
@@ -79,13 +21,100 @@ struct PostDetail: View {
         }
         .onAppear { if post.deleted { dismiss() } }
         .onReceive(post.$deleted) { if $0 { dismiss() } }
-        .sheet(isPresented: $showingEditor) {
-            PostEditor(post: post)
+        .sheet(isPresented: $showingEditor) { PostEditor(post: post) }
+        .toolbar { Button("Edit") { showingEditor = true } }
+    }
+
+    @ViewBuilder
+    private var created: some View {
+        Timestamp(
+            prefix: "Posted",
+            systemImage: "clock",
+            date: post.created
+        )
+    }
+
+    @ViewBuilder
+    private var description: some View {
+        if let description = post.description {
+            Text(description)
+                .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .toolbar {
-            Button("Edit") {
-                showingEditor.toggle()
+    }
+
+    @ViewBuilder
+    private var metadata: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            created
+            modified
+            objectCount
+            tags
+        }
+        .padding()
+    }
+
+    @ViewBuilder
+    private var modified: some View {
+        if post.created != post.modified {
+            Timestamp(
+                prefix: "Last modified",
+                systemImage: "pencil",
+                date: post.modified
+            )
+        }
+    }
+
+    @ViewBuilder
+    private var objects: some View {
+        if !post.objects.isEmpty {
+            ObjectGrid(objects: post.objects)
+        }
+    }
+
+    @ViewBuilder
+    private var objectCount: some View {
+        if !post.objects.isEmpty {
+            Label(post.objects.countOf(type: "Object"), systemImage: "doc")
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+
+    @ViewBuilder
+    private var postInfo: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            title
+            description
+        }
+        .padding()
+
+        objects
+        metadata
+    }
+
+    @ViewBuilder
+    private var tags: some View {
+        if post.tags.count > 1 {
+            NavigationLink(destination: TagList(post: post)) {
+                Label(post.tags.countOf(type: "Tag"), systemImage: "tag")
+                    .font(.caption)
             }
+        }
+        else if let tag = post.tags.first {
+            NavigationLink(destination: TagDetail(tag: tag, repo: post.repo)) {
+                Label(tag.name, systemImage: "tag")
+                    .font(.caption)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var title: some View {
+        if let title = post.title {
+            Text(title)
+                .bold()
+                .font(.title2)
+                .frame(maxWidth: .infinity, alignment: .leading)
         }
     }
 
