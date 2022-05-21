e843e178fbfea0a33b3ee2fbe156f67ae522ed27
Gene Koval <genyaremchuk@protonmail.com>
2022-05-20 20:26:51 -0400

    Use SheetView to implement CommentEditor

--- Shared/Views/Comments/CommentEditor.swift
+++ Shared/Views/Comments/CommentEditor.swift
@@ -1,46 +1,33 @@
 import SwiftUI
 
 struct CommentEditor: View {
-    @Environment(\.dismiss) var dismiss
-
     let type: String
+
     @Binding var draft: String
+
     let onDone: () -> Void
 
     var body: some View {
-        NavigationView {
+        SheetView(title: "\(type) Comment", done: (
+            label: "Done",
+            action: onDone,
+            disabled: { draft.isWhitespace }
+        )) {
             TextEditor(text: $draft)
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationTitle("\(type) Comment")
                 .padding(.horizontal)
-                .toolbar {
-                    ToolbarItem(placement: .primaryAction) { doneButton }
-                    ToolbarItem(placement: .cancellationAction) { cancelButton }
-                }
         }
-    }
-
-    @ViewBuilder
-    private var cancelButton: some View {
-        Button("Cancel") { dismiss() }
-    }
-
-    @ViewBuilder
-    private var doneButton: some View {
-        Button(action: { done() }) {
-            Text("Done")
-                .bold()
-        }
-        .disabled(draft.isWhitespace)
-    }
-
-    private func done() {
-        onDone()
-        dismiss()
     }
 }
 
 struct CommentEditor_Previews: PreviewProvider {
+    private struct Preview: View {
+        @State private var draft = ""
+
+        var body: some View {
+            CommentEditor(type: "New", draft: $draft) { }
+        }
+    }
+
     static var previews: some View {
         CommentEditor(type: "New", draft: .constant("")) { }
     }
