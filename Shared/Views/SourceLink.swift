c4a423ca7e1cc1ee0f6fb0321a5f1fbc8879c504
Gene Koval <genyaremchuk@protonmail.com>
2023-06-25 03:14:46 -0400

    Add ability to copy and share Source links

--- Shared/Views/SourceLink.swift
+++ Shared/Views/SourceLink.swift
@@ -13,13 +13,36 @@ struct SourceLink: View {
             .aspectRatio(contentMode: .fit)
             .frame(width: 15)
 
-            Link(destination: URL(string: source.url)!) {
+            Link(destination: url) {
                 ScrollView(.horizontal, showsIndicators: false) {
                     Text(source.url)
                 }
             }
         }
         .font(.caption)
+        .contextMenu {
+            Button {
+                UIPasteboard.general.string = source.url
+            } label: {
+                Label("Copy Link", systemImage: "doc.on.doc")
+            }
+
+            ShareLink(item: url)
+        } preview: {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(url.host!)
+                    .bold()
+
+                Text(url.absoluteString)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 30)
+        }
+    }
+
+    private var url: URL {
+        URL(string: source.url)!
     }
 }
 
